e4c07983d341ce4813a533d70ceb543b11499324
VirLg <19virlg@gmail.com>
2024-04-22 13:06:48 +0300

    add start files

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+root['bg'] = '#fafafa'
+root.title("Working space")
+root.wm_attributes("-alpha", 0.9)
+root.geometry("800x800") 
+root.resizable(width=False, height =False )
+
+
+root.mainloop()
